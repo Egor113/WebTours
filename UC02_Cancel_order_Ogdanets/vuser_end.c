42cb46e5fb8dc90efa3760481a8834076c6be0b3
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-11-30 19:56:26 +0300

    Last commit

--- UC02_Cancel_order_Ogdanets/vuser_end.c
+++ UC02_Cancel_order_Ogdanets/vuser_end.c
@@ -3,11 +3,11 @@ vuser_end()
 	lr_start_transaction("UC02_TR04_Logout");
 
 	web_url("SignOff Button", 
-		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"URL={Host}:{Port}/cgi-bin/welcome.pl?signOff=1", 
 		"TargetFrame=body", 
 		"Resource=0", 
 		"RecContentType=text/html", 
-		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Referer={Host}:{Port}/cgi-bin/nav.pl?page=menu&in=itinerary", 
 		"Snapshot=t12.inf", 
 		"Mode=HTML", 
 		LAST);
